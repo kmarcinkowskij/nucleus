ac353dee5f107e214c7062e3f6c5ce38fa849b49
bigboyd14 <wojtysiaczek06@gmail.com>
2025-12-02 11:27:08 +0100

    started on inventory

--- scenes/ray_cast_3d.gd
+++ scenes/ray_cast_3d.gd
@@ -1,16 +1,45 @@
 extends RayCast3D
+@export var main_scene: Node3D;
 
-@export var n_INVENTORY_NODE: Node3D
+var equippable_objects = ["dick", "balls", "shaft"];
+var inventory_array = ["0", "0"];
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+var selected = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#if(Input.is_action_just_pressed("drop_item")):
+		#if(inventory_array[selected] == "0"):
+			#return
+		#inventory_array[selected] = "0"
+	
 	if(is_colliding() && Input.is_action_pressed('interact')):
-		n_INVENTORY_NODE.add_child(get_collider())
-		get_collider().queue_free()
+		force_raycast_update()
+		if(get_collider() == null):
+			return
+		if(!get_collider().has_meta('name')):
+			return
+			
+		var current_looked = get_collider().get_meta("name")
+		var current_looked2 = get_collider()
+		
+		if current_looked == equippable_objects[0] or equippable_objects[1] or equippable_objects[2]:
+			print("looking at " + current_looked)
+			if(inventory_array[0] == "0"):
+				inventory_array[0] = current_looked
+				print("currently held in slot 1 is item: " + inventory_array[0])
+				current_looked2.queue_free()
+			elif(inventory_array[1] == "0"):
+				inventory_array[1] = current_looked
+				print("currently held in slot 2 is item: " + inventory_array[1])
+				current_looked2.queue_free()
+			else:
+				print("retarded")
+			
+			
 		
 		
 	
