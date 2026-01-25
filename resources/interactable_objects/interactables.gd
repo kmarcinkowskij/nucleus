c2fe35c2ebb3aa4a46b7ec937e9fb10e0ba3493f
kmarcinkowskij <km.dartmoor3@gmail.com>
2026-01-25 22:36:00 +0100

    breakable wall basic

--- resources/interactable_objects/interactables.gd
+++ resources/interactable_objects/interactables.gd
@@ -2,12 +2,13 @@ class_name interactables
 extends Node3D
 
 var object_scenes: Dictionary = {
-	0: load("res://resources/interactable_objects/interactable_0.tscn"),
-	1: load("res://resources/interactable_objects/interactable_1.tscn"),
-	2: load("res://resources/interactable_objects/interactable_2.tscn"),
-	3: load("res://resources/interactable_objects/interactable_3.tscn"),
-	4: load("res://resources/interactable_objects/medkit.tscn"),
-	5: load("res://resources/interactable_objects/medkit_big.tscn")
+	0: load("res://resources/interactable_objects/interactable_0.tscn"), #test interactable
+	1: load("res://resources/interactable_objects/interactable_1.tscn"), #test interactable
+	2: load("res://resources/interactable_objects/interactable_2.tscn"), #test interactable
+	3: load("res://resources/interactable_objects/interactable_3.tscn"), #test interactable
+	4: load("res://resources/interactable_objects/medkit.tscn"),		 #a basic medkit that heals 20hp 
+	5: load("res://resources/interactable_objects/medkit_big.tscn"),	 #a bigger variant of the medkit that heals 50hp 
+	6: load("res://resources/interactable_objects/wall_breakable.tscn")	 #a breakable wall
 	}
 # Called when the node enters the scene tree for the first time.
 
